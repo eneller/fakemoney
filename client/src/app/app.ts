886903e4029eeccf6806060873d399e2eb0b200d
eneller <erikneller@gmx.de>
2026-03-05 20:09:15 +0100

    feat: working nav

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,21 +2,19 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {
-	NgbNavContent,
 	NgbNav,
 	NgbNavItem,
 	NgbNavItemRole,
-	NgbNavLinkButton,
 	NgbNavLinkBase,
-	NgbNavOutlet,
 } from '@ng-bootstrap/ng-bootstrap/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgbModule, NgbNavContent, NgbNav, NgbNavItem, NgbNavItemRole, NgbNavLinkButton, NgbNavLinkBase, NgbNavOutlet],
+  imports: [RouterOutlet, NgbModule, NgbNav, NgbNavItem, NgbNavItemRole, NgbNavLinkBase],
   templateUrl: './app.html',
   styleUrl: './app.less'
 })
 export class App {
   protected readonly title = signal('client');
+  active = 1;
 }
